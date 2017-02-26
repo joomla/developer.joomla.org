a8e6f65037d3bb4dde688e7dd26a90236f9326b9
Michael Babker <mbabker@flbab.com>
2017-02-26 10:40:16 -0600

    Remove build tool and ignore

--- plugins/content/plg_content_nightlybuilds/build/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -rf packaging && rm -f plg_content_nightlybuilds.zip && mkdir packaging
-cp -r language/ packaging/language/
-cp -r *.php packaging/
-cp -r nightlybuilds.xml packaging/
-cd packaging/
-zip -r ../plg_content_nightlybuilds.zip *
