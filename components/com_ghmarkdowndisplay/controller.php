11e6eca02481e8b333c9240bfbe6ea1793eb58d9
Michael Babker <michael.babker@gmail.com>
2018-07-28 22:49:13 -0500

    Handle redirect to 'default' document if needed, wire up component view cache config

--- components/com_ghmarkdowndisplay/controller.php
+++ components/com_ghmarkdowndisplay/controller.php
@@ -9,7 +9,9 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Router\Route;
 
 /**
  * Component base controller.
@@ -22,4 +24,51 @@ class GHMarkdownDisplayController extends BaseController
 	 * @var  string
 	 */
 	protected $default_view = 'document';
+
+	/**
+	 * Method to display a view.
+	 *
+	 * @param   boolean  $cachable   If true, the view output will be cached
+	 * @param   array    $urlparams  An array of safe URL parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
+	 *
+	 * @return  $this
+	 */
+	public function display($cachable = false, $urlparams = [])
+	{
+		// Set the default view name and format from the Request.
+		$vName = $this->input->get('view', $this->default_view);
+		$this->input->set('view', $vName);
+
+		/*
+		 * Handle a case where the document view is loaded with a repository ID but no item ID,
+		 * generally this should be from a menu item and this will prevent duplicate URLs.
+		 */
+		if ($vName === 'document' && $this->input->getUint('id') === null)
+		{
+			/** @var GHMarkdownDisplayModelDocument $model */
+			$model = $this->getModel('Document');
+			$item  = $model->getItem();
+
+			// If no item is given, the view will handle errors
+			if ($item !== false)
+			{
+				// Handle redirecting to the canonical document URL
+				$this->setRedirect(Route::_('index.php?option=com_ghmarkdowndisplay&view=document&id=' . $item->id));
+
+				return $this;
+			}
+		}
+
+		$cachable = !Factory::getUser()->guest;
+
+		$urlparams = [
+			'id'         => 'UINT',
+			'repository' => 'UINT',
+			'print'      => 'BOOLEAN',
+			'lang'       => 'CMD',
+			'Itemid'     => 'UINT',
+		];
+
+		return parent::display($cachable, $urlparams);
+	}
 }
